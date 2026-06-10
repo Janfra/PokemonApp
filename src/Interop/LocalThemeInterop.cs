04002af6b1d029b6f2bdab57e17c037ee7f3d519
Jan <janfrancoherrera@gmail.com>
2026-06-10 20:46:25 +0100

    feat: add interop window access for theme
    # Description
    Added editing the document classes to add and remove dark class for toggling dark mode.
    
    Added function to check browser theme preference to fallback on.
    
    > [!WARNING]
    > Changes made directly to the document on the client do not persist since changes are not applied on the server, and since the server provides the page body, the changes are lost. Keep for reference purposes.

---
+++ src/Interop/LocalThemeInterop.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.JSInterop;
+
+namespace PokemonApp.Interop;
+
+public class LocalThemeInterop : ILocalThemeInterop
+{
+    public bool IsDarkMode { get; private set; }
+    private readonly IJSRuntime _javaScript;
+    private const string _setThemeIdentifier = "LocalTheme.setIsDarkMode";
+    private const string _isBrowserDarkModeIdentifier = "LocalTheme.isBrowserDarkMode";
+
+    public LocalThemeInterop(IJSRuntime javaScript)
+    {
+        _javaScript = javaScript;
+    }
+
+    public async Task SetIsDarkMode(bool isDarkMode)
+    {
+        IsDarkMode = isDarkMode;
+        await _javaScript.InvokeVoidAsync(_setThemeIdentifier, isDarkMode);
+    }
+
+    public async Task<bool> IsBrowserDarkMode()
+    {
+        return await _javaScript.InvokeAsync<bool>(_isBrowserDarkModeIdentifier);
+    }
+}
---
+++ src/wwwroot/js/LocalTheme.js
@@ -0,0 +1,13 @@
+﻿window.LocalTheme = {
+    setIsDarkMode: function (isDarkMode) {
+        if (isDarkMode) {
+            window.document.body.classList.add("dark");
+        } else {
+            window.document.body.classList.remove("dark");
+        }
+    },
+    isBrowserDarkMode: function () {
+        const prefersDarkQuery = "(prefers-color-scheme: dark)"
+        return window.matchMedia(prefersDarkQuery).matches
+    }
+}
