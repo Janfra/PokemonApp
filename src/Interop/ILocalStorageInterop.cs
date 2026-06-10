329201d2d3c87eb6820fdcc0432af077f8b07716
Jan <janfrancoherrera@gmail.com>
2026-06-10 20:42:16 +0100

    feat: add interop access to local storage
    # Description
    Added javascript folder for accessing local storage to save theme preferences.

---
+++ src/Interop/ILocalStorageInterop.cs
@@ -0,0 +1,11 @@
+﻿
+namespace PokemonApp.Interop
+{
+    public interface ILocalStorageInterop
+    {
+        Task ClearData();
+        Task<string> GetItem(string key);
+        Task RemoveItem(string key);
+        Task SetItem(string key, string value);
+    }
+}
---
+++ src/Interop/LocalStorageInterop.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.JSInterop;
+
+namespace PokemonApp.Interop;
+
+public class LocalStorageInterop : ILocalStorageInterop
+{
+    private readonly IJSRuntime _javaScript;
+    private const string _getItemIdentifier = "LocalStorageActions.getItem";
+    private const string _setItemIdentifier = "LocalStorageActions.setItem";
+    private const string _removeItemIdentifier = "LocalStorageActions.removeItem";
+    private const string _clearDataIdentifier = "LocalStorageActions.clearData";
+
+    public LocalStorageInterop(IJSRuntime javaScript)
+    {
+        _javaScript = javaScript;
+    }
+
+    public async Task<string> GetItem(string key)
+    {
+        return await _javaScript.InvokeAsync<string>(_getItemIdentifier, key);
+    }
+
+    public async Task SetItem(string key, string value)
+    {
+        await _javaScript.InvokeVoidAsync(_setItemIdentifier, key, value);
+    }
+
+    public async Task RemoveItem(string key)
+    {
+        await _javaScript.InvokeVoidAsync(_removeItemIdentifier, key);
+    }
+
+    public async Task ClearData()
+    {
+        await _javaScript.InvokeVoidAsync(_clearDataIdentifier);
+    }
+}
