576950bfdb66f339941b9db346e2846626f655b9
Jan <janfrancoherrera@gmail.com>
2026-07-01 13:26:30 +0100

    feat: add memory cache to pokemon service
    # Description
    Responses from PokéAPI are now cached in memory to reduce API calls on recently fetched data.

--- src/Services/PokemonService.cs
+++ src/Services/PokemonService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.Extensions.Caching.Memory;
 using PokemonApp.Models;
 using System.Collections.Concurrent;
 
@@ -7,31 +8,49 @@ namespace PokemonApp.Services;
 public class PokemonService : IPokemonService
 {
     private readonly HttpClient _httpClient;
+    private readonly MemoryCache _memoryCache;
 
     public PokemonService(HttpClient httpClient)
     {
         _httpClient = httpClient;
+
+        var memoryOptions = new MemoryCacheOptions();
+        memoryOptions.ExpirationScanFrequency = TimeSpan.FromMinutes(3);
+        memoryOptions.SizeLimit = 1024;
+        _memoryCache = new MemoryCache(memoryOptions);
     }
 
     public async Task<Results<Ok<Pokemon>, NotFound>> GetPokemonAsync(string name)
     {
+        if (_memoryCache.TryGetValue(name, out Pokemon? cachedPokemon))
+        {
+            return TypedResults.Ok(cachedPokemon);
+        }   
+
         var pokemon = await _httpClient.GetFromJsonAsync<Pokemon>($"pokemon/{name.ToLower()}");
         if (pokemon is null)
         {
             return TypedResults.NotFound();
         }
 
+        _memoryCache.Set(name, pokemon);
         return TypedResults.Ok(pokemon);
     }
 
     public async Task<Results<Ok<Pokemon>, NotFound>> GetPokemonAsync(int id)
     {
+        if (_memoryCache.TryGetValue(id, out Pokemon? cachedPokemon))
+        {
+            return TypedResults.Ok(cachedPokemon);
+        }
+
         var pokemon = await _httpClient.GetFromJsonAsync<Pokemon>($"pokemon/{id}");
         if (pokemon is null)
         {
             return TypedResults.NotFound();
         }
 
+        _memoryCache.Set(id, pokemon);
         return TypedResults.Ok(pokemon);
     }
 
@@ -48,11 +67,18 @@ public class PokemonService : IPokemonService
 
         await Parallel.ForEachAsync(response.Results, parallelOptions, async (resource, cancellationToken) =>
         {
+            if (_memoryCache.TryGetValue(resource.Name, out Pokemon? cachedPokemon))
+            {
+                pokemons.Add(cachedPokemon!);
+                return;
+            }
+
             var p = await _httpClient.GetFromJsonAsync<Pokemon>(new Uri(resource.Url), cancellationToken);
             if (p is not null)
             {
                 pokemons.Add(p);
             }
+            _memoryCache.Set(resource.Name, p);
         });
 
         if (pokemons.IsEmpty)
